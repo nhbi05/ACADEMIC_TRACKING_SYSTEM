5e26df709c560f58ea3655c104e0a55e079d5e49
KEWODA JOANITAH <kewodagoanitah@gmail.com>
2025-03-29 20:51:07 +0300

    RESLOVED CONFLICTS

--- aits-frontend/src/context/AuthContext.js
+++ aits-frontend/src/context/AuthContext.js
@@ -4,20 +4,34 @@ import React, { createContext, useState, useContext } from 'react';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [user, setUser] = useState(prev => {
+    // This is a quick fix probably use sessions
+    try {
+      return JSON.parse(localStorage.getItem('user'))
+    } catch {}
+    return null
+  });
+  const [token, setToken] = useState(prev => {
+    try {
+      return JSON.parse(localStorage.getItem('token'))
+    } catch {}
+    return null
+  });
 
-  const login = (userData, token) => {
-    setUser(userData);
+  const login = (user, token) => {
+    setUser(user);
     setToken(token);
-    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('token', JSON.stringify(token));
   };
 
   const logout = () => {
     setUser(null);
     setToken(null);
+    localStorage.removeItem('user');
     localStorage.removeItem('token');
   };
+  // console.log(user ? "Reloading..." : "Bootstarped", {user,token})
 
   return (
     <AuthContext.Provider value={{ user, token, login, logout }}>
