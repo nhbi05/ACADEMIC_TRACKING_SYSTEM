611d9d368e70a25a47fb5a943dd14a922f45f40f
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-04-02 18:27:50 +0300

    frontend branch

--- backend/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Issue, Notification
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Issue)
-admin.site.register(Notification)
-
