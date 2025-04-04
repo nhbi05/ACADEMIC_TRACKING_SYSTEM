886956441a9b1d1c47f4822bac1a32fac4eaa831
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-04-04 21:43:36 +0300

    merging to main

--- backend/AITS_project/settings.py
+++ backend/AITS_project/settings.py
@@ -17,7 +17,7 @@ from unittest.mock import DEFAULT
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for procduction
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
