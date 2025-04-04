9d783b56e87f0576504ba17473770f759aed7411
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-04-04 11:07:31 +0300

    resolving merge conflicts

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -55,10 +55,7 @@ class Migration(migrations.Migration):
             name='Issue',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-<<<<<<< HEAD
                 ('registration_no', models.CharField(max_length=20)),
-=======
->>>>>>> raymond-branch
                 ('category', models.CharField(choices=[('missing_marks', 'Missing Marks'), ('appeal', 'Appeal'), ('correction', 'Correction'), ('others', 'Others')], max_length=100)),
                 ('status', models.CharField(choices=[('pending', 'pending'), ('in_progress', 'In Progress'), ('resolved', 'Resolved')], default='pending', max_length=20)),
                 ('description', models.TextField()),
