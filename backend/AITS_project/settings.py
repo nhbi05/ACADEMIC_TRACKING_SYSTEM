0769626b1117ab58336731e3539657b18c959372
Ray007-XP <raymondsuuna05@gmail.com>
2025-04-09 22:30:27 +0300

    Update settings.py

--- backend/AITS_project/settings.py
+++ backend/AITS_project/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'AITS_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ait_db',
+        'NAME': 'railway',
         'USER':'postgres',
         'PASSWORD':db_password,
         'HOST':db_host,
@@ -177,7 +177,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER ')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
