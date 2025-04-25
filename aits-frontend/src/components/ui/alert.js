cb0f285f7f3e692c376b105941b6c2ba7dc7d17c
KewodaJoanitah <kewodagoanitah@gmail.com>
2025-04-26 01:37:42 +0300

    modified alert.js

--- aits-frontend/src/components/ui/alert.js
+++ aits-frontend/src/components/ui/alert.js
@@ -13,7 +13,7 @@ export const Alert = ({ variant = "default", children }) => {
   };
 
   return (
-    <div className={`${baseStyle} ${variantStyles[variant]}`}>
+    <div className={`${baseStyles} ${variantStyles[variant]}`}>
       {children}
     </div>
   );
