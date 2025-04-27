0dfaa3ca2dc45c696f0cb87738754eaa72f5f8a2
Your Name <nabukeerasumayah354@gmail.com>
2025-04-27 13:26:58 +0300

    change

--- backend/users/views.py
+++ backend/users/views.py
@@ -297,7 +297,7 @@ class StudentIssueView(generics.ListAPIView):
 
 
 
-class ResolvedIssuesView(generics.ListAPIView): #if the issue is solved
+class ResolvedIssuesView(generics.ListAPIView): #
     serializer_class = IssueSerializer
     permission_classes = [IsAuthenticated]
 
