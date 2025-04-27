adc4f15c7a846a42bd63844196c224a8e238d777
Your Name <nabukeerasumayah354@gmail.com>
2025-04-27 12:54:06 +0300

    change

--- backend/AITS_project/settings.py
+++ backend/AITS_project/settings.py
@@ -102,13 +102,13 @@ DATABASES = {
 
 
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 
-}"""
+}
 
 DATABASES = {
     'default': {
