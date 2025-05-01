94e84198b6babd642d7b38415d806d0546f27e44
denvalpact <tdenisvalelian@gmail.com>
2025-05-01 14:57:34 +0300

    commit

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from .models import Issue,User
 from .models import Issue,User, StudentProfile, LecturerProfile
 from rest_framework.response import Response
-from rest_framework import status,generics,filters
+from rest_framework import status,generics,filter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.utils.decorators import method_decorator
