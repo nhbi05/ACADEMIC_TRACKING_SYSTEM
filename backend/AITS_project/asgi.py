0c8df4150ab3b77aeddd808dd23430b6c6602098
denvalpact <tdenisvalelian@gmail.com>
2025-05-01 14:41:01 +0300

    l commit

--- backend/AITS_project/asgi.py
+++ backend/AITS_project/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AITS_project.settings')
 
-application = get_asgi_application(git 
+application = get_asgi_application(git)
