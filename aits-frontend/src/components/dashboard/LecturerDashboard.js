dc2d8f3597710df5968077ee260f36d1665ac8fa
KewodaJoanitah <kewodagoanitah@gmail.com>
2025-05-04 19:29:31 +0300

    modified lecturer dashboard

--- aits-frontend/src/components/dashboard/LecturerDashboard.js
+++ aits-frontend/src/components/dashboard/LecturerDashboard.js
@@ -8,7 +8,7 @@ const LecturerDashboard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   
-  // Add the missing useState variables
+  // Add the Missing useState variables
   const [activeTab, setActiveTab] = useState('assigned');
   const [selectedIssue, setSelectedIssue] = useState(null);
   
