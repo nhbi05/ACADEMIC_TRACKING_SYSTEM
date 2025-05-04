bcbe98f6a66c176740ab73950a0799ef9f2445ca
KewodaJoanitah <kewodagoanitah@gmail.com>
2025-05-04 19:50:48 +0300

    modified lecturer dashboard

--- aits-frontend/src/components/dashboard/LecturerDashboard.js
+++ aits-frontend/src/components/dashboard/LecturerDashboard.js
@@ -15,7 +15,7 @@ const LecturerDashboard = () => {
   // Get User From Redux State
   const { user } = useSelector(state => state.auth || { user: null });
   
-  // Log the Entire Lecturer state for debugging
+  // Log the Entire Lecturer State for debugging
   const lecturerState = useSelector(state => state.lecturer);
   console.log('Full lecturer state:', lecturerState);
   
