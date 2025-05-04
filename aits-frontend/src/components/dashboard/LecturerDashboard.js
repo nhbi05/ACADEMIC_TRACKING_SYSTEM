48a25323b9249e823aa8d02959da3dd84d944d94
KewodaJoanitah <kewodagoanitah@gmail.com>
2025-05-04 19:57:10 +0300

    modified lecturer dashboard

--- aits-frontend/src/components/dashboard/LecturerDashboard.js
+++ aits-frontend/src/components/dashboard/LecturerDashboard.js
@@ -26,7 +26,7 @@ const LecturerDashboard = () => {
     }
   });
 
-  // Log the Extracted Data For debugging
+  // Log the Extracted Data For Debugging
   console.log('Issues from state:', issues);
   console.log('Resolved issues from state:', resolvedIssues);
 
