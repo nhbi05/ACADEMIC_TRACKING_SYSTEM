c9e9841835b03b6d71d1012bc1e4e31afe969186
KewodaJoanitah <kewodagoanitah@gmail.com>
2025-05-13 16:33:14 +0300

    modified student  dashboard

--- aits-frontend/src/components/dashboard/StudentDashboard.js
+++ aits-frontend/src/components/dashboard/StudentDashboard.js
@@ -193,7 +193,7 @@ const navItems = [
           </div>
         </header>
         
-        {/* MAIN DASHBOard Content */}
+        {/* MAIN DASHBOArd Content */}
         <main className="p-6">
           {error && (
             <div className="bg-red-50 text-red-700 p-4 rounded-md mb-6">
